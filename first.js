1af8c1a71fb86ae7a08e9b4f76fbb0885980893e
avishka8 <avishka1108@gmail.com>
2025-01-18 19:26:39 +0530

    js

--- first.js
+++ first.js
@@ -8,3 +8,15 @@ x=null;
 console.log(x);
 y=undefined;
 console.log(y);
+//we use let(can be updated), const(cannot be updated) nowadays because they cannot be re-declared unlike var//
+ let z=BigInt("1234");
+ const student = {
+    name: "Avishka Srivastava",
+    years: 21,
+    college: "VIT",
+ }
+ console.log(student);
+ console.log(student["years"]);
+ console.log(student.college);
+ student["years"]=student["years"] + 1;
+ console.log(student.years);
