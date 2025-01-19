e7fd8094499dd026572ac02d466f0c6a36c60eb6
avishka8 <avishka1108@gmail.com>
2025-01-19 15:22:20 +0530

    practice q2

--- pracques.js
+++ pracques.js
@@ -1,6 +1,17 @@
+//ball pen on an e-commerce data//
 const product = {
     pName:"Ball Pen",
     pRating: 4.5,
     pPrice: "$24",
     pOffer: 0.05
+}
+
+//storing data for a social media profile//
+const profile={
+    username: avishhkaa,
+    followers:800,
+    following:1000,
+    posts:3,
+    bio:"no need to play safe",
+    follow:true
 }
